d28b7c303a96194672c1c4a1990a48cc67dd2fc3
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-16 16:05:16 +0300

    Добавлен случай когда пациент выписан

--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -8,7 +8,9 @@ def test_get_status():
     hospital = Hospital([1, 3])
     assert hospital.get_status(1) == "Болен"
 
-def test_get_status_when_patient_missing():
-    hospital = Hospital([0, 2])
+
+@pytest.mark.parametrize('patient_id', [10, 2])
+def test_get_status_when_patient_missing(patient_id):
+    hospital = Hospital([0, None, 2])
     with pytest.raises(PatientMissingError):
-        hospital.get_status(10)
+        hospital.get_status(patient_id)
