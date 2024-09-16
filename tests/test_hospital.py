1a5eef5f6f029eadef886f4da8cacb0d9b4820e4
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-16 15:38:17 +0300

    Изменены тестовые данные

--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -4,7 +4,7 @@ from hospital import Hospital
 
 
 def test_get_status():
-    hospital = Hospital()
+    hospital = Hospital([1, 3])
     assert hospital.get_status(1) == "Болен"
 
 def test_get_status_when_patient_missing():
