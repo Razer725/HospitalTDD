0e7ac7cbb79f22a1de24d1e2cfb1f54a4232fded
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-16 16:02:41 +0300

    Добавлена обработка случая когда индекс больше длины списка

--- hospital.py
+++ hospital.py
@@ -1,3 +1,6 @@
+from exceptions import PatientMissingError
+
+
 class Hospital:
     STATUSES = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен", 3: "Готов к выписке"}
 
@@ -7,4 +10,10 @@ class Hospital:
         self.patients = patients
 
     def get_status(self, patient_id):
-        return self.STATUSES[self.patients[patient_id - 1]]
+        inner_id = self._convert_patient_id_to_inner_id(patient_id)
+        return self.STATUSES[self.patients[inner_id]]
+
+    def _convert_patient_id_to_inner_id(self, patient_id):
+        if patient_id > len(self.patients):
+            raise PatientMissingError
+        return patient_id - 1
