d995a1792bee9483b81840546e0a512280dca86f
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-16 14:46:59 +0300

    Добавлен test_get_status_when_patient_missing

--- tests/test_hospital.py
+++ tests/test_hospital.py
@@ -1,6 +1,13 @@
+import pytest
+
 from hospital import Hospital
 
 
 def test_get_status():
     hospital = Hospital()
     assert hospital.get_status(1) == "Болен"
+
+def test_get_status_when_patient_missing():
+    hospital = Hospital([0, 2])
+    with pytest.raises(PatientMissingError):
+        hospital.get_status(10)
