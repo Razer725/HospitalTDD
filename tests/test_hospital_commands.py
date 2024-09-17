1d6c8936014a800cb5ed7ba09c06c9e24f1d7fa1
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-17 16:53:30 +0300

    Добавлен test_status_up_with_max_status_when_discharge_confirmed

--- tests/test_hospital_commands.py
+++ tests/test_hospital_commands.py
@@ -44,3 +44,13 @@ def test_status_up():
     assert hospital.patients == [2, 3]
     dialog_with_user.send_message.assert_called_once_with('Новый статус пациента: "Слегка болен"')
 
+def test_status_up_with_max_status_when_discharge_confirmed():
+    dialog_with_user = MagicMock()
+    hospital = Hospital([1, 3])
+    hospital_commands = HospitalCommands(hospital, dialog_with_user)
+    dialog_with_user.request_patient_id = MagicMock(return_value=2)
+    dialog_with_user.request_discharge_confirmation = MagicMock(return_value=True)
+
+    hospital_commands.status_up()
+    assert hospital.patients == [1, None]
+    dialog_with_user.send_message.assert_called_once_with('Пациент выписан из больницы')
