ac306b628eac716e2d920b8e6bbd86457d9d91af
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-17 16:17:15 +0300

    Добавлена test_status_up

--- tests/test_hospital_commands.py
+++ tests/test_hospital_commands.py
@@ -33,3 +33,14 @@ def test_get_status_when_patient_missing():
 
     hospital_commands.get_status()
     dialog_with_user.send_message.assert_called_once_with("Ошибка. В больнице нет пациента с таким ID")
+
+def test_status_up():
+    dialog_with_user = MagicMock()
+    hospital = Hospital([1, 3])
+    hospital_commands = HospitalCommands(hospital, dialog_with_user)
+    dialog_with_user.request_patient_id = MagicMock(return_value=1)
+
+    hospital_commands.status_up()
+    assert hospital.patients == [2, 3]
+    dialog_with_user.send_message.assert_called_once_with('Новый статус пациента: "Слегка болен"')
+
