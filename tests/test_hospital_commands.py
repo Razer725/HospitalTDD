e61c15e54450d69e7a0b6f8b33228e46024bd350
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-17 12:55:16 +0300

    Добавлен test_get_status

---
+++ tests/test_hospital_commands.py
@@ -0,0 +1,13 @@
+from unittest.mock import MagicMock
+
+from hospital import Hospital
+
+
+def test_get_status():
+    dialog_with_user = MagicMock()
+    hospital =  Hospital([1, 3])
+    hospital_commands = HospitalCommands(hospital, dialog_with_user)
+    dialog_with_user.request_patient_id = MagicMock(return_value=1)
+
+    hospital_commands.get_status()
+    dialog_with_user.send_message.assert_called_once_with("Болен")
