35c406a1317fa037aa2e3c1935fd80f2decc629a
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-17 18:15:40 +0300

    Добавлен test_status_up_when_id_type_invalid

--- tests/test_hospital_commands.py
+++ tests/test_hospital_commands.py
@@ -23,7 +23,8 @@ def test_get_status_when_id_type_invalid():
     dialog_with_user.request_patient_id = MagicMock(side_effect=PatientIDTypeError)
 
     hospital_commands.get_status()
-    dialog_with_user.send_message.assert_called_once_with("Ошибка. ID пациента должно быть числом (целым, положительным)")
+    dialog_with_user.send_message.assert_called_once_with(
+        "Ошибка. ID пациента должно быть числом (целым, положительным)")
 
 def test_get_status_when_patient_missing():
     dialog_with_user = MagicMock()
@@ -65,3 +66,13 @@ def test_status_up_with_max_status_when_discharge_declined():
     hospital_commands.status_up()
     assert hospital.patients == [1, 3]
     dialog_with_user.send_message.assert_called_once_with('Пациент остался в статусе "Готов к выписке"')
+
+def test_status_up_when_id_type_invalid():
+    dialog_with_user = MagicMock()
+    hospital = Hospital([1, 3])
+    hospital_commands = HospitalCommands(hospital, dialog_with_user)
+    dialog_with_user.request_patient_id = MagicMock(side_effect=PatientIDTypeError)
+
+    hospital_commands.status_up()
+    dialog_with_user.send_message.assert_called_once_with(
+        "Ошибка. ID пациента должно быть числом (целым, положительным)")
