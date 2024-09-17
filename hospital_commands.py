ba2508b8cde2a61406cde2b8cfc070e0c410dc80
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-17 14:35:29 +0300

    Добавлена обработка PatientIDTypeError

--- hospital_commands.py
+++ hospital_commands.py
@@ -1,9 +1,15 @@
+from exceptions import PatientIDTypeError
+
+
 class HospitalCommands:
     def __init__(self, hospital, dialog_with_user):
         self.hospital = hospital
         self.dialog_with_user = dialog_with_user
 
     def get_status(self):
-        patient_id = self.dialog_with_user.request_patient_id()
-        status = self.hospital.get_status(patient_id)
-        self.dialog_with_user.send_message(status)
+        try:
+            patient_id = self.dialog_with_user.request_patient_id()
+            status = self.hospital.get_status(patient_id)
+            self.dialog_with_user.send_message(status)
+        except PatientIDTypeError:
+            self.dialog_with_user.send_message("Ошибка. ID пациента должно быть числом (целым, положительным)")
