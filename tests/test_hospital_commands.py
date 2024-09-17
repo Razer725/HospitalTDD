4bec70729f093bd7bf679067f4c01c87b5ddc957
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-17 14:00:34 +0300

    Добавлен test_get_status_when_id_type_invalid

--- tests/test_hospital_commands.py
+++ tests/test_hospital_commands.py
@@ -1,5 +1,7 @@
 from unittest.mock import MagicMock
 
+import pytest
+
 from hospital_commands import HospitalCommands
 from hospital import Hospital
 
@@ -12,3 +14,12 @@ def test_get_status():
 
     hospital_commands.get_status()
     dialog_with_user.send_message.assert_called_once_with("Болен")
+
+def test_get_status_when_id_type_invalid():
+    dialog_with_user = MagicMock()
+    hospital = Hospital([1, 3])
+    hospital_commands = HospitalCommands(hospital, dialog_with_user)
+    dialog_with_user.request_patient_id = MagicMock(return_value=PatientIDTypeError)
+
+    with pytest.raises(PatientIDTypeError):
+        hospital_commands.get_status()
