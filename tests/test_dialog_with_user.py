34dfd2f4ee7bb736154a3f4ddc8b8a438d4d1280
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-18 15:23:53 +0300

    Добавлен test_request_patient_id_wit_invalid_type

--- tests/test_dialog_with_user.py
+++ tests/test_dialog_with_user.py
@@ -1,9 +1,20 @@
 from unittest.mock import patch
 
+import pytest
+
 from dialog_with_user import DialogWithUser
+from exceptions import PatientIDTypeError
 
 
-@patch('builtins.input', return_value='1')
-def test_request_patient_id(mock_input):
+def test_request_patient_id(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: '1')
     dialog_with_user = DialogWithUser()
     assert dialog_with_user.request_patient_id() == 1
+
+
+@pytest.mark.parametrize('user_input', ['0', '-1', 'два', '1.1'])
+def test_request_patient_id_wit_invalid_type(monkeypatch, user_input):
+    monkeypatch.setattr('builtins.input', lambda _: user_input)
+    dialog_with_user = DialogWithUser()
+    with pytest.raises(PatientIDTypeError):
+        dialog_with_user.request_patient_id()
