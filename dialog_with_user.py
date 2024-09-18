76af6ac1acf7a2cdc70f165b6d9b75447278ab73
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-18 15:30:07 +0300

    Добавлена обработка некорректных значений patient id

--- dialog_with_user.py
+++ dialog_with_user.py
@@ -1,3 +1,12 @@
+from exceptions import PatientIDTypeError
+
+
 class DialogWithUser:
     def request_patient_id(self):
-        return int(input("Введите ID пациента: "))
+        patient_id = input("Введите ID пациента: ")
+        if not patient_id.isdigit() or int(patient_id) == 0:
+            raise PatientIDTypeError
+
+        return int(patient_id)
+
+
