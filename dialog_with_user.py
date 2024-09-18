49295058abbb2533f98224dbd7fb55a5f02674e0
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-18 14:47:16 +0300

    Добавлен request_patient_id

---
+++ dialog_with_user.py
@@ -0,0 +1,3 @@
+class DialogWithUser:
+    def request_patient_id(self):
+        return int(input("Введите ID пациента: "))
--- tests/test_dialog_with_user.py
+++ tests/test_dialog_with_user.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch
 
+from dialog_with_user import DialogWithUser
+
 
 @patch('builtins.input', return_value='1')
 def test_request_patient_id(mock_input):
