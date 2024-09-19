9b223e82b73a5179fce594dd19222814d91234ab
Ivan Krasilnikov <ia.krasilnikov@rt-solar.ru>
2024-09-19 15:33:32 +0300

    Добавлена test_discharge

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -4,8 +4,25 @@ from hospital_commands import HospitalCommands
 
 
 def test_discharge():
-    dialog_with_user = DialogWithUser()
-    hospital = Hospital()
+    console = MockConsole()
+    dialog_with_user = DialogWithUser(console)
+    hospital = Hospital([0, 3])
     hospital_commands = HospitalCommands(hospital, dialog_with_user)
-    application = Application(hospital_commands)
+    application = Application(hospital_commands, dialog_with_user)
+
+    console.add_expected_request_and_response('Введите команду: ', 'discharge')
+    console.add_expected_request_and_response('Введите ID пациента: ', '1')
+    console.add_expected_output_message('Пациент выписан из больницы')
+
+    console.add_expected_request_and_response('Введите команду: ', 'discharge')
+    console.add_expected_request_and_response('Введите ID пациента: ', '1')
+    console.add_expected_output_message('Ошибка. В больнице нет пациента с таким ID')
+
+    console.add_expected_request_and_response('Введите команду: ', 'стоп')
+    console.add_expected_output_message('Сеанс завершён.')
+
+    application.run()
+
+    console.verify_all_calls_have_been_made()
+    assert hospital.patients == [None, 3]
 
